4f2856c97eacd202f5cd70f756698cf22381597c
mark <gardiolamark2003@gmail.com>
2025-03-20 12:09:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ListGroup from './components/ListGroup'
+
+const App = () => {
+  return (
+    <div>
+      <ListGroup/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+      <h1>Lorem Ipsum</h1>
+    </div>
+  )
+}
+
+export default Message
